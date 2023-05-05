9a8bed0c1b37eb223901e92aecf20744f3fef722
ductruong253 <ddtruong94@gmail.com>
2023-05-06 06:52:31 +0700

    added db coonectiong setting

--- dist/src/app.module.js
+++ dist/src/app.module.js
@@ -13,6 +13,7 @@ const app_service_1 = require("./app.service");
 const config_1 = require("@nestjs/config");
 const configuration_1 = require("../config/configuration");
 const auth_middleware_1 = require("./interceptors/auth.middleware");
+const typeorm_1 = require("@nestjs/typeorm");
 const cookieSession = require('cookie-session');
 let AppModule = class AppModule {
     configure(consumer) {
@@ -23,10 +24,22 @@ let AppModule = class AppModule {
 };
 AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [config_1.ConfigModule.forRoot({
+        imports: [
+            config_1.ConfigModule.forRoot({
                 load: [configuration_1.default],
                 isGlobal: true
-            })],
+            }),
+            typeorm_1.TypeOrmModule.forRoot({
+                type: 'postgres',
+                host: 'localhost',
+                port: 5432,
+                username: 'postgres',
+                password: 'admin',
+                database: 'coupon-info',
+                synchronize: true,
+                autoLoadEntities: true
+            })
+        ],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,26 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import config from '../config/configuration'
 import { AuthMiddleWare } from './interceptors/auth.middleware';
+import { TypeOrmModule } from '@nestjs/typeorm';
 const cookieSession = require('cookie-session');
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    load: [config],
-    isGlobal: true
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      load: [config],
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin',
+      database: 'coupon-info',
+      synchronize: true,
+      autoLoadEntities: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
